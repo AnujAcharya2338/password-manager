b16a451b319b9497a3a85bf7c2938a5b3bdf1060
AnujAcharya2338 <anujacharya877@gmail.com>
2025-08-11 08:44:29 -0700

    Half completed

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    with open("data.txt", "w") as f:
+        output1=input1.get()
+        output2=input2.get()
+        output3=input3.get()
+        f.write(output1)
+        f.write(output2)
+        f.write(output3)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -11,7 +21,7 @@ window = Tk()
 window.title("Password manager")
 window.config(padx=20,pady=20)
 
-canvas = Canvas(width=200,height=200, highlightthickness=0)
+canvas = Canvas(width=200,height=200)
 photo = PhotoImage(file="logo.png")
 canvas.create_image(100,100,image=photo)
 canvas.grid(row=0,column=1)
@@ -29,9 +39,10 @@ password.grid(row=3,column=0)
 
 input1 = Entry(width=36)
 input1.grid(row=1,column=1, columnspan=2)
-
+input1.focus()
 input2 = Entry(width=36)
 input2.grid(row=2,column=1,columnspan=2)
+input2.insert(0,"anujacharya877@gmail.com")
 
 
 input3 = Entry(width=28)
@@ -40,6 +51,6 @@ input3.grid(row=3,column=1)
 generate=Button(text="Generate Password")
 generate.grid(row=3,column=2)
 
-add=Button(text="Add", width=36)
+add=Button(text="Add", width=36,command=save)
 add.grid(row=4,column=1,columnspan=2)
 window.mainloop()
