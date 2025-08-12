58e2fe95359e6f26497d9e87d4b813792857fcab
AnujAcharya2338 <anujacharya877@gmail.com>
2025-08-12 08:22:45 -0700

    Day 30 exception handling in python using try except else and finally

---
+++ day30.py
@@ -0,0 +1,22 @@
+# try:
+#     file = open("a_filw.txt")
+# except FileNotFoundError:
+#     file = open("a_filw.txt", "w")
+#     file.write("Something")
+# else:
+#     content = file.read()
+#     print(content)
+    
+# finally:
+#     raise TypeError("i Created this erroor")
+
+
+# height = float(input("Height: "))
+# weight = int(input("weight: "))
+
+
+# if height > 3:
+#     raise ValueError("Wrong height")
+
+# bmi = weight / height **2
+# print(bmi)
